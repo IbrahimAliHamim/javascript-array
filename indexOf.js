88684639e5078aeb06abb3b9e4458b0cb41776a9
Ibrahim Ali Hamim <ibrahimalihamim@gmail.com>
2023-09-15 01:16:22 +0600

    JS indexOf

---
+++ indexOf.js
@@ -0,0 +1,10 @@
+// ------1____
+
+// const fruits = ["Mango", "Jackfruit", "Guava", "Apple", "Grape", "Tangerine", "Orange"];
+
+// let fruit = fruits.indexOf("Mango");
+// console.log(fruit);
+
+const fruits = ["Mango", "Jackfruit", "Guava", "Apple", "Grape", "Tangerine", "Orange"];
+const fruit = fruits.indexOf("Guava", 3);
+console.log(fruit);
