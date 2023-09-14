8208fed3d07621e2cc1bfa0ce09848eb6d96002e
Ibrahim Ali Hamim <ibrahimalihamim@gmail.com>
2023-09-15 01:29:37 +0600

    JS Length

--- indexOf.js
+++ indexOf.js
@@ -5,6 +5,6 @@
 // let fruit = fruits.indexOf("Mango");
 // console.log(fruit);
 
-const fruits = ["Mango", "Jackfruit", "Guava", "Apple", "Grape", "Tangerine", "Orange"];
-const fruit = fruits.indexOf("Guava", 3);
-console.log(fruit);
+// const fruits = ["Mango", "Jackfruit", "Guava", "Apple", "Grape", "Tangerine", "Orange"];
+// const fruit = fruits.indexOf("Guava", 3);
+// console.log(fruit);
