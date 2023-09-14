1e309bd2395a98302f512e8704d8238d02f738b8
Ibrahim Ali Hamim <ibrahimalihamim@gmail.com>
2023-09-14 11:28:34 +0600

    js-fill2nds

--- fill.js
+++ fill.js
@@ -1,3 +1,12 @@
-const fruits = ["Mango", "Jackfruit", "Guava", "Apple"];
-fruits.fill("Tangerine");
-console.log(fruits)
+// ---------1______
+
+// const fruits = ["Mango", "Jackfruit", "Guava", "Apple"];
+// fruits.fill("Tangerine");
+// console.log(fruits);
+
+// -----------2______
+
+// const fruits = ["Mango", "Jackfruit", "Guava", "Apple"];
+// fruits.fill("Tangerine", 1, 4);
+
+// console.log(fruits)
